202dfea1c669503c0dc4a67ca9366f7fa0def49d
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2022-01-02 16:14:57 +0700

    implement async testing

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
-export const fetchMovies = function () {
+import fetch from 'node-fetch';
+
+export const fetchMovies = () => {
   return fetch("http://www.alanfoster.me/movies.json").then((response) =>
     response.json()
   );
+};
+
+export const fetchMockData = (callback) => {
+  setTimeout(() => callback("Phat trien web nang cao"), 1000);
 };
 
 export const add = (a, b) => {
--- test-suite/async.test.js
+++ test-suite/async.test.js
@@ -1,5 +1,22 @@
+import { fetchMockData } from "../functions";
+
 describe("suite 2", function () {
   test("test 2.1", () => {
     expect(2 + 3).toBe(5);
+  });
+});
+
+describe("async test", () => {
+  test("callback test", (done) => {
+    function callback(data) {
+      try {
+        expect(data).toBe("Phat trien web nang cao")
+        done();
+      } catch (err) {
+        done(err);
+      }
+    }
+
+    fetchMockData(callback);
   });
 });
