6ab3a43571b5a87bcc0324aea04dc6f0304946bc
Nguyen Van Duc <nguyenduc21022k@gmail.com>
2022-01-02 21:10:08 +0700

    property matcher

--- test-suite/snapShot.test.js
+++ test-suite/snapShot.test.js
@@ -2,9 +2,24 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Link from "../react/Link";
 
-it("renders correctly", () => {
-  const tree = renderer
-    .create(<Link href="http://www.facebook.com" label="Facebook"></Link>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+describe("snapshot test", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Link href="http://www.facebook.com" label="Facebook"></Link>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("property matchers", () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      createdAt: new Date(),
+      name: "test user",
+    };
+
+    expect(user).toMatchSnapshot({
+      createdAt: expect.any(Date),
+      id: expect.any(Number)
+    });
+  })
 });
